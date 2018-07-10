65aa349a318dfd931796498665588fbb4e0149fd
Sam Chung <tieoneease@gmail.com>
2018-07-09 22:04:40 -0700

    el fin

--- src/index.js
+++ src/index.js
@@ -1,297 +1,320 @@
 import Vue from 'vue/dist/vue.js';
+import semantic from '../semantic/semantic.js';
+import '../semantic/semantic.css';
 
-var products = [
-  {
-    name: 'SkullCandy Blazin\' Buds',
-    attributes: {
-      'Price': '$250',
-      'Rating': '3/5',
-      '# Ratings': '359'
-    }
-  },
-  {
-    name: 'Bose QuietComfort 25',
-    attributes: {
-      'Price': '$350',
-      'Rating': '4/5',
-      '# Ratings': '359'
-    }
-  },
-]
-
-var tasks = [
+function initQuestionMatrix(initData) {
+  var __products = [
     {
-        "Question": "",
-        "Answers": "",
-        "Tokens": "1"
+      name: 'SkullCandy Blazin\' Buds',
+      attributes: {
+        'Price': '$250',
+        'Rating': '3/5',
+        '# Ratings': '359',
+        'Colour': 'Red'
+      }
     },
-  {
-    "Question": "The cow jumped over the ____",
-    "Answers": "moon, Moon",
-    "Tokens": "1"
-  },
-  {
-    "Question": "What is the colour of the sky?",
-    "Answers": "blue, Blue, black, Black",
-    "Tokens": "2"
-  }
-]
-
-Vue.component('product-attribute-row', {
-  props: ['attribute', 'unlockedMap', 'unlockAttribute'],
-  computed: {
-    unlocked() {
-      return this.unlockedMap[this.attribute.name];
-    }
-  },
-  template: `
-    <tr @click="unlockRow(attribute.name)">
-      <td>{{ attribute.name }}</td>
-      <td v-for="value in attribute.values">
-        {{ unlocked ? value : '' }}
-      </td>
-    </tr>
-  `,
-  methods: {
-    unlockRow: function(attributeName) {
-      this.unlockAttribute(attributeName);
+    {
+      name: 'Bose QuietComfort 25',
+      attributes: {
+        'Price': '$350',
+        'Rating': '4/5',
+        '# Ratings': '359',
+        'Colour': 'Blue'
+      }
     },
-  }
-})
+  ]
 
-// products => render
-// state of visibility
-Vue.component('product-table', {
-  props: ['unlockedMap', 'unlockAttribute'],
-  data: function () {
-    return {
-      products: products
-    }
-  },
-  computed: {
-    productAttributes: function () {
-      let _products = this.products;
-      let attributeNames = Object.keys(products[0].attributes);
-      return attributeNames.map(attributeName => {
-        return {
-          name: attributeName,
-          values: _products.map(product => product.attributes[attributeName])
-        }
-      });
-    }
-  },
-  template: `
-          <table class="ui selectable celled definition table">
-            <thead>
-              <tr><th></th>
-                <th v-for="product in products">
-                    {{product.name}}
-                </th>
-              </tr></thead>
-              <tbody>
-                <product-attribute-row
-                  v-for="attribute in productAttributes"
-                  v-bind:attribute="attribute"
-                  v-bind:unlockAttribute="unlockAttribute"
-                  v-bind:unlockedMap="unlockedMap"
-                  v-bind:key="attribute.name">
-                </product-attribute-row>
-              </tbody>
-          </table>
-  `
-})
-
-Vue.component('task-card', {
-    props: ['task', 'index', 'setUnlockable', 'renderTask'],
-    data() {
-        return {
-            taskInstance: Object.assign(this.task, {}),
-            taskTokens: parseInt(this.task.Tokens),
-            redeemableTokens: 0,
-            currentlyRedeeming: false,
-            doneRedeeming: false
-        }
+  var __tasks = [
+    {
+      "Question": "",
+      "Answers": "",
+      "Tokens": "1",
+      "Hidden": "No"
     },
+    {
+      "Question": "The cow jumped over the ____",
+      "Answers": "moon, Moon",
+      "Tokens": "1",
+      "Hidden": "No"
+    },
+    {
+      "Question": "What is the colour of the sky?",
+      "Answers": "blue, Blue, black, Black",
+      "Tokens": "2",
+      "Hidden": "Yes"
+    }
+  ]
+
+  Vue.component('product-attribute-row', {
+    props: ['attribute', 'unlockedMap', 'unlockAttribute'],
     computed: {
-        answers() {
-            return new Set(this.taskInstance.Answers.split(',')
-                .map(answer => answer.trim())
-                .map(answer => answer.toLowerCase()));
-        },
-        redeemable() {
-            if (this.taskInstance.Question.trim().length == 0) {
-                this.redeemableTokens = this.taskTokens;
-                return true;
-            }
-
-            return this.redeemableTokens > 0;
-        },
-        header() {
-            if (this.doneRedeeming) {
-                return 'Unlocked ' + this.taskTokens + ' Attribute' + (this.taskTokens > 1 ? 's' : '');
-            }
-
-            if (this.redeemable) {
-                return 'Unlock ' + this.redeemableTokens + ' Attribute' + (this.redeemableTokens > 1 ? 's' : '');
-            }
-
-            return 'Complete Task to Unlock ' + this.taskTokens + ' Attribute' + (this.taskTokens > 1 ? 's' : '');
-        },
-        buttonText() {
-            if (this.currentlyRedeeming) {
-                return 'Redeeming...'
-            }
-
-            if (this.doneRedeeming) {
-                return 'Redeemed'
-            }
-
-            if (this.redeemable) {
-                return 'Redeem'
-            }
-
-            return 'Complete Task ' + this.index;
-        }
-    },
-    methods: {
-        redeemToken() {
-            this.currentlyRedeeming = true;
-            this.setUnlockable(this);
-        },
-        finishRedeeming() {
-            this.currentlyRedeeming = false;
-            this.redeemableTokens--;
-
-            if (this.redeemableTokens == 0)
-                this.doneRedeeming = true;
-        },
-        startTask() {
-            if (this.doneRedeeming)
-                return;
-
-            this.renderTask(this);
-        },
-        validateAnswer(answer) {
-            if (this.answers.has(answer)) {
-                this.redeemableTokens = this.taskTokens;
-                return true;
-            }
-
-            return false;
-        }
+      unlocked() {
+        return this.unlockedMap[this.attribute.name];
+      }
     },
     template: `
-            <div class="ui card">
-              <div class="content">
-                <div class="header"> {{ header }}</div>
-                <div class="description">
-                  <p></p>
-                </div>
-              </div>
-              <div id="redeemable"
-                  class="ui bottom attached button"
-                  v-bind:class="{ positive: redeemable && !doneRedeeming, disabled: doneRedeeming }"
-                  v-on="{ click: redeemable && !doneRedeeming ? redeemToken : startTask }">
-                {{ buttonText }}
-              </div>
-            </div>
-    `
-})
+      <tr @click="unlockRow(attribute.name)">
+        <td>{{ attribute.name }}</td>
+        <td v-for="value in attribute.values">
+          {{ unlocked ? value : '' }}
+        </td>
+      </tr>
+    `,
+    methods: {
+      unlockRow: function(attributeName) {
+        this.unlockAttribute(attributeName);
+      },
+    }
+  })
 
-// tasks => render
-// state of tasks
-Vue.component('task-cards', {
-    props: ['setUnlockable', 'renderTask'],
-    data() {
-        return {
-            tasks: tasks
-        }
+  Vue.component('product-table', {
+    props: ['unlockedMap', 'unlockAttribute'],
+    data: function () {
+      return {
+        products: __products
+      }
     },
-  template: `
-          <div class="ui cards">
-              <task-card
-                v-for="(task, index) in tasks"
-                v-bind:task="task"
-                v-bind:index="index"
-                v-bind:key="index"
-                v-bind:setUnlockable="setUnlockable"
-                v-bind:renderTask="renderTask"/>
-          </div>
-`
-})
+    computed: {
+      productAttributes: function () {
+        let _products = this.products;
+        let attributeNames = Object.keys(_products[0].attributes);
+        return attributeNames.map(attributeName => {
+          return {
+            name: attributeName,
+            values: _products.map(product => product.attributes[attributeName])
+          }
+        });
+      }
+    },
+    template: `
+        <table class="ui selectable celled definition table">
+          <thead>
+            <tr><th></th>
+              <th v-for="product in products">
+                  {{product.name}}
+              </th>
+            </tr></thead>
+            <tbody>
+              <product-attribute-row
+                v-for="attribute in productAttributes"
+                v-bind:attribute="attribute"
+                v-bind:unlockAttribute="unlockAttribute"
+                v-bind:unlockedMap="unlockedMap"
+                v-bind:key="attribute.name">
+              </product-attribute-row>
+            </tbody>
+        </table>
+    `
+  })
 
-// products => render
-// state of visibility
-Vue.component('task-modal', {
-  props: ['task', 'submitTask', 'closeModal', 'taskActive'],
+  Vue.component('task-card', {
+    props: ['task', 'index', 'setUnlockable', 'renderTask', 'incrementTasksDone'],
     data() {
-        return {
-            answer: '',
-            activeClass: 'transition visible active',
-            activeDimmerStyle: {
-                display: 'flex !important'
-            },
-            activeModalStyle: {
-                display: 'block !important'
-            },
-            answeredCorrectly: true
+      return {
+        taskInstance: Object.assign(this.task, {}),
+        taskTokens: parseInt(this.task.Tokens),
+        redeemableTokens: 0,
+        currentlyRedeeming: false,
+        doneRedeeming: false
+      }
+    },
+    created() {
+      if (this.taskInstance.Question.trim().length == 0)
+        this.incrementTasksDone();
+    },
+    computed: {
+      answers() {
+        return new Set(this.taskInstance.Answers.split(',')
+          .map(answer => answer.trim())
+          .map(answer => answer.toLowerCase()));
+      },
+      redeemable() {
+        if (this.taskInstance.Question.trim().length == 0) {
+          this.redeemableTokens = this.taskTokens;
+          return true;
         }
+
+        return this.redeemableTokens > 0;
+      },
+      header() {
+        if (this.doneRedeeming) {
+          return 'Unlocked ' + this.taskTokens + ' Attribute' + (this.taskTokens > 1 ? 's' : '');
+        }
+
+        if (this.redeemable) {
+          return 'Unlock ' + this.redeemableTokens + ' Attribute' + (this.redeemableTokens > 1 ? 's' : '');
+        }
+
+        return 'Complete Task to Unlock ' + this.taskTokens + ' Attribute' + (this.taskTokens > 1 ? 's' : '');
+      },
+      buttonText() {
+        if (this.currentlyRedeeming) {
+          return 'Redeeming...'
+        }
+
+        if (this.doneRedeeming) {
+          return 'Redeemed'
+        }
+
+        if (this.redeemable) {
+          return 'Redeem'
+        }
+
+        return 'Complete Task ' + this.index;
+      }
     },
     methods: {
-        checkAnswer() {
-            this.answeredCorrectly = this.task.validateAnswer(this.answer);
-            if (this.answeredCorrectly) {
-                this.answer = '';
-                this.closeModal();
-            }
+      redeemToken() {
+        this.currentlyRedeeming = true;
+        this.setUnlockable(this);
+      },
+      finishRedeeming() {
+        this.currentlyRedeeming = false;
+        this.redeemableTokens--;
+
+        if (this.redeemableTokens == 0)
+          this.doneRedeeming = true;
+      },
+      startTask() {
+        if (this.doneRedeeming)
+          return;
+
+        this.renderTask(this);
+      },
+      validateAnswer(answer) {
+        if (this.answers.has(answer)) {
+          this.redeemableTokens = this.taskTokens;
+          return true;
         }
+
+        return false;
+      }
     },
-  template: `
-    <div
-      v-bind:class="[taskActive ? activeClass : '']"
-      v-bind:style="[taskActive ? activeDimmerStyle : '']"
-      class="ui dimmer modals page">
-        <div id="task-modal"
+    template: `
+      <div class="ui card">
+        <div class="content">
+          <div class="header"> {{ header }}</div>
+          <div class="description">
+            <p></p>
+          </div>
+        </div>
+        <div 
+            class="ui bottom attached button"
+            v-bind:class="{ positive: redeemable && !doneRedeeming, disabled: doneRedeeming }"
+            v-on="{ click: redeemable && !doneRedeeming ? redeemToken : startTask }">
+          {{ buttonText }}
+        </div>
+      </div>
+    `
+  })
+
+  Vue.component('task-cards', {
+    props: ['setUnlockable', 'renderTask', 'numTasksDone', 'incrementTasksDone'],
+    data() {
+      return {
+        tasks: __tasks
+      }
+    },
+    computed: {
+    },
+    methods: {
+      showTask(task, index) {
+        let hidden = task.Hidden.trim().toLowerCase() == 'yes';
+        if (hidden) {
+          if (this.numTasksDone >= index)
+            return true
+
+          return false;
+        }
+
+        return true;
+      }
+    },
+    template: `
+      <div class="ui cards">
+          <task-card
+            v-for="(task, index) in tasks"
+            v-bind:task="task"
+            v-bind:index="index"
+            v-bind:key="index"
+            v-bind:setUnlockable="setUnlockable"
+            v-bind:renderTask="renderTask"
+            v-bind:incrementTasksDone="incrementTasksDone"
+            v-if="showTask(task, index)"
+          />
+      </div>`
+  })
+
+  Vue.component('task-modal', {
+    props: ['task', 'closeModal', 'taskActive', 'incrementTasksDone'],
+      data() {
+        return {
+          answer: '',
+          activeClass: 'transition visible active',
+          activeDimmerStyle: {
+            display: 'flex !important'
+          },
+          activeModalStyle: {
+            display: 'block !important'
+          },
+          answeredCorrectly: true
+        }
+      },
+      methods: {
+        checkAnswer() {
+          let answer = this.answer.trim().toLowerCase();
+          this.answeredCorrectly = this.task.validateAnswer(answer);
+          if (this.answeredCorrectly) {
+            this.answer = '';
+            this.closeModal();
+            this.incrementTasksDone();
+          }
+        }
+      },
+    template: `
+      <div
+        v-bind:class="[taskActive ? activeClass : '']"
+        v-bind:style="[taskActive ? activeDimmerStyle : '']"
+        class="ui dimmer modals page">
+          <div 
             v-bind:class="[taskActive ? activeClass : '']"
             v-bind:style="[taskActive ? activeModalStyle : '']"
-            class="ui mini modal"
-            >
-            <i @click="closeModal" class="close icon"></i>
-            <div id="modal-header" class="header">
-                Task
-            </div>
-            <div class="content">
-                <div class="description">
-                    <div class="ui header">Answer the question</div>
-                    <p id="modal-text">{{ task.taskInstance ? task.taskInstance.Question : '' }}</p>
-                    <div v-bind:class="{ error: !answeredCorrectly }" class="ui input">
-                        <input @keyup.enter="checkAnswer" type="text" placeholder="Answer" v-model="answer">
-                    </div>
-                </div>
-                <p v-if="!answeredCorrectly">Please try again.</p>
-            </div>
-            <div class="actions">
-                <div @click="closeModal" class="ui negative deny button">
-                   Cancel 
-                </div>
-                <div @click="checkAnswer" id="finish-task" class="ui positive button">
-                    Submit
-                </div>
-            </div>
-        </div>
-    </div>
-  `
-})
-
-function init() {
+            class="ui mini modal">
+              <i @click="closeModal" class="close icon"></i>
+              <div  class="header">
+                  Task
+              </div>
+              <div class="content">
+                  <div class="description">
+                      <div class="ui header">Answer the question</div>
+                      <p>{{ task.taskInstance ? task.taskInstance.Question : '' }}</p>
+                      <div v-bind:class="{ error: !answeredCorrectly }" class="ui input">
+                          <input @keyup.enter="checkAnswer" type="text" placeholder="Answer" v-model="answer">
+                      </div>
+                  </div>
+                  <p v-if="!answeredCorrectly">Please try again.</p>
+              </div>
+              <div class="actions">
+                  <div @click="closeModal" class="ui negative deny button">
+                     Cancel 
+                  </div>
+                  <div @click="checkAnswer" class="ui positive button">
+                      Submit
+                  </div>
+              </div>
+          </div>
+      </div>
+    `
+  })
   var app = new Vue({
-    el: '#app',
+    el: '#question-matrix-app',
     data: {
       unlockable: false,
       unlockedMap: {},
       redeemingTask: {},
-        taskActive: false,
-        openTask: {}
+      taskActive: false,
+      openTask: {},
+      numTasksDone: 0
     },
     template: `
       <div>
@@ -299,6 +322,7 @@ function init() {
             v-bind:task="openTask"
             v-bind:taskActive="taskActive"
             v-bind:closeModal="closeModal"
+            v-bind:incrementTasksDone="incrementTasksDone"
         >
         </task-modal>
         <div class="ui stackable very relaxed grid container">
@@ -310,13 +334,14 @@ function init() {
           <div class="four wide column">
             <task-cards
                 v-bind:setUnlockable="setUnlockable"
-                v-bind:renderTask="renderTask"/>
+                v-bind:renderTask="renderTask"
+                v-bind:numTasksDone="numTasksDone"
+                v-bind:incrementTasksDone="incrementTasksDone"
+            />
           </div>
         </div>
       </div>
     `,
-    mounted: function() {
-    },
     methods: {
       unlockAttribute: function(attributeName) {
         if (this.unlockable && !this.unlockedMap[attributeName]) {
@@ -336,9 +361,12 @@ function init() {
       renderTask: function(task) {
         this.openTask = task;
         this.taskActive = true;
+      },
+      incrementTasksDone: function() {
+        this.numTasksDone++;
       }
     }
   })
 }
 
-document.addEventListener("DOMContentLoaded", init);
+window.initQuestionMatrix = initQuestionMatrix;
--- webpack.config.js
+++ webpack.config.js
@@ -27,11 +27,20 @@ module.exports = {
       {
         test: /\.css$/,
         use: [ 'style-loader', 'css-loader' ]
-      }
+      },
+      {test:/\.svg$/,loader:'url-loader',query:{mimetype:'image/svg+xml',name:'./public/css/semantic/themes/default/assets/fonts/icons.svg'}},
+
+      {test:/\.woff$/,loader:'url-loader',query:{mimetype:'application/font-woff',name:'./public/css/semantic/themes/default/assets/fonts/icons.woff'}},
+
+      {test:/\.woff2$/,loader:'url-loader',query:{mimetype:'application/font-woff2',name:'./public/css/semantic/themes/default/assets/fonts/icons.woff2'}},
+
+      {test:/\.[ot]tf$/,loader:'url-loader',query:{mimetype:'application/octet-stream',name:'./public/css/semantic/themes/default/assets/fonts/icons.ttf'}},   
+
+      {test:/\.eot$/,loader:'url-loader',query:{mimetype:'application/vnd.ms-fontobject',name:'./public/css/semantic/themes/default/assets/fonts/icons.eot'}}
     ]
   },
   output: {
-    filename: 'bundle.js',
+    filename: 'matrix-bundle.js',
     path: path.resolve(__dirname, 'dist')
   }
 };
